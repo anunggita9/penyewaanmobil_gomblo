6ac1ea35e84b45607337c9a77b888c5cbac62c2b
yoganungita <121782512+anunggita9@users.noreply.github.com>
2023-01-03 13:59:35 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,6 +2,6 @@
     $hostname = 'localhost';
     $username = 'root';
     $password = '';
-    $dbname = 'penyewaanmobil';
+    $dbname = 'penyewaanmobil_gomblo';
     $conn = mysqli_connect($hostname, $username, $password, $dbname) or die ('Gagal terhubung ke databse');
 ?>
